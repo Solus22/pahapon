55c3deae6e14e02f25107eca8bf632a91036c17e
ololosh-oldy <166790964+ololosh-oldy@users.noreply.github.com>
2024-04-12 11:26:24 +0500

    Update asdsfad.py

--- asdsfad.py
+++ asdsfad.py
@@ -1,8 +1,11 @@
-print reeeeeeeee
-def gopstop
-int(rot
-ramdom (iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
-import lelogret
+print("reeeeeeeee")
+from random import*
+def gopstop():
+  rot = int(input())
+  for i in range(rot):
+      print("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii"*random() )
+  return random ()
+#import lelogret
 saa = 33030
 
 
